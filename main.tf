cb978813ab749eed56ded1435ead12328ee541a8
Damian Kasyan <damian.kasyan@hotmail.com>
2022-05-16 17:28:07 +0200

    Add aws instance

--- main.tf
+++ main.tf
@@ -10,26 +10,23 @@ resource "aws_vpc" "myvpc" {
   
 }
 
-resource "aws_ec2" "ec2" {
+resource "aws_instance" "ec2" {
     ami = "ami-00c90dbdc12232b58"
-
-  
+    instance_type = "t2.micro"
 }
 #output "vpcid" {
 #    value = aws_vpc.myvpc.myvpc.id
      
 #}
-
-variable "multiple" {
-  type = tuple([string, number, string])
-  default = ["cat", 1, "dog"]
-}
-
-variable "myobject" {
-    type = object({name = string, port = list(number) })
-    default = {
-      name = "TJ"
-      port = [22, 25, 80]
-    }
+#variable "multiple" {
+#  type = tuple([string, number, string])
+#  default = ["cat", 1, "dog"]
+#}
+#variable "myobject" {
+ #   type = object({name = string, port = list(number) })
+  #  default = {
+  #    name = "TJ"
+  #    port = [22, 25, 80]
+  #  }
   
-}
+#}
