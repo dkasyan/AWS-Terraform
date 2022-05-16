6fd969ea180f63dd68cbdad45fb05a100523cae8
Damian Kasyan <damian.kasyan@hotmail.com>
2022-05-16 21:33:19 +0200

    Add sec group

--- main.tf
+++ main.tf
@@ -4,10 +4,32 @@ provider "aws" {
 resource "aws_instance" "ec2" {
     ami = "ami-00c90dbdc12232b58"
     instance_type = "t2.micro"
+#    security_groups = [aws_security_group.webtraffic.name]
 }
 resource "aws_eip" "elasticeip" {
     instance = aws_instance.ec2.id
     
+}
+resource "aws_security_group" "webtraffic" {
+    name = "Allow HTTPS"
+    ingress {
+        from_port = 443
+        to_port = 443
+        protocol = "TCP"
+        cidr_blocks = ["0.0.0.0/0"]
+
+
+    }
+    egress {
+        from_port = 443
+        to_port = 443
+        protocol = "TCP"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
+
+resource "aws_vpc" "myvpc" {
+    cidr_block = "10.0.0.0/16"
 }
 output "EIP" {
     value = aws_eip.elasticeip.public_ip
