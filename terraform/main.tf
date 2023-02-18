c0692be9b7b8526a9c522150649d573c69893b3f
Damian Kasyan <damian.kasyan@hotmail.com>
2023-02-18 20:37:17 +0000

    Comment online backend

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,13 +1,13 @@
 terraform {
   # Run init/plan/apply with "backend" commented-out (ueses local backend) to provision Resources (Bucket, Table)
   # Then uncomment "backend" and run init, apply after Resources have been created (uses AWS)
-  backend "s3" {
-    bucket         = "cc-tf-state-backend-ci-cd"
-    key            = "tf-infra/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "terraform-state-locking"
-    encrypt        = true
-  }
+  # backend "s3" {
+  #   bucket         = "cc-tf-state-backend-ci-cd"
+  #   key            = "tf-infra/terraform.tfstate"
+  #   region         = "eu-west-1"
+  #   dynamodb_table = "terraform-state-locking"
+  #   encrypt        = true
+  # }
 
   required_version = ">=0.13.0"
   required_providers {
@@ -27,12 +27,12 @@ module "tf-state" {
   bucket_name = "iac-tf-state-backend-ci-cd"
 }
 
-module "vpc-infra" {
-  source = "./modules/vpc"
+# module "vpc-infra" {
+#   source = "./modules/vpc"
 
-  # VPC Input Vars
-  vpc_cidr             = local.vpc_cidr
-  availability_zones   = local.availability_zones
-  public_subnet_cidrs  = local.public_subnet_cidrs
-  private_subnet_cidrs = local.private_subnet_cidrs
-}
+#   # VPC Input Vars
+#   vpc_cidr             = local.vpc_cidr
+#   availability_zones   = local.availability_zones
+#   public_subnet_cidrs  = local.public_subnet_cidrs
+#   private_subnet_cidrs = local.private_subnet_cidrs
+# }
